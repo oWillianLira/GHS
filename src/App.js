149f90fd0c3a65eeb4a804036e64d3e72ab4ffbe
Willian Lira <55150867+frontneon@users.noreply.github.com>
2020-07-11 16:20:51 -0300

    new installation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Header from './components/header'
+
+function App() {
+  return (
+    <>
+    <Header />
+    </>
+  );
+}
+
+export default App;
